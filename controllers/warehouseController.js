461d0ba76d324d7f0ffb9d8ef155557bf37b44e7
Joshua Jeyaranjan <joshuajeyaranjan@gmail.com>
2023-11-07 21:30:44 -0500

    create fetch warehouse by id controller

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -6,7 +6,22 @@ const db = knex(knexConfig);
 exports.getAllWarehouses = async (_req, res) => {};
 
 exports.getWarehouse = async (req, res) => {
-  const { warehouseId } = req.params; //Stores warehouse id in url
+  try {
+    const { warehouseId } = req.params;
+                          //NOT SURE IF IM CALLING THIS DB OR KNEX
+    const warehouse = await db('warehouses')
+      .where({ id: warehouseId })
+      .first();
+
+    if (!warehouse) {
+      return res.status(404).json({ message: 'Warehouse not found' });
+    }
+
+    return res.status(200).json(warehouse);
+  } catch (error) {
+
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
 exports.createWarehouse = async (req, res) => {

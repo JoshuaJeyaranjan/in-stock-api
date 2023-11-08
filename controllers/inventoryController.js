0fc7f55365f29bb90502df14b88f120d4337f860
Joshua Jeyaranjan <joshuajeyaranjan@gmail.com>
2023-11-07 21:47:21 -0500

    add controller for getting all inventory items

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,6 +1,22 @@
 // Inventory controller functions go here
 
-exports.getAllInventoryItems = async (req, res) => {};
+exports.getAllInventoryItems = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const inventoryItems = await db('inventories')
+      .where({ warehouse_id: id });
+
+    if (inventoryItems.length === 0) {
+      return res.status(404).json({ message: 'No inventory items found for this warehouse' });
+    }
+
+    return res.status(200).json(inventoryItems);
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
 
 exports.getInventoryItem = async (req, res) => {
   const { itemId } = req.params; //Stores item id in url

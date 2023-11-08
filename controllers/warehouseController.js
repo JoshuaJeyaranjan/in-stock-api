f38d7cbb2e85414578464fb4b417f9dcd6c39f8a
Leslie Al-Hassan <leslie.k.alhassan@gmail.com>
2023-11-07 21:13:10 -0500

    Change module format from ES6 to Common JS

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -3,14 +3,7 @@ const knex = require("knex");
 const knexConfig = require("../knexfile");
 const db = knex(knexConfig);
 
-exports.getAllWarehouses = async (_req, res) => {
-  try {
-    const data = await db("warehouses");
-    return res.status(200).json(data);
-  } catch (error) {
-    res.status(400).send({ error: err });
-  }
-};
+exports.getAllWarehouses = async (_req, res) => {};
 
 exports.getWarehouse = async (req, res) => {
   const { warehouseId } = req.params; //Stores warehouse id in url
